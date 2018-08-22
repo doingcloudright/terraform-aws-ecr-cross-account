399cc8ed9871143fde4613642570b1da221f4318
maartenvanderhoef <maarten@blinkist.com>
2018-08-22 17:33:33 +0200

    Moved lifecycle policy creation to a different module for more straight forward ux.

--- main.tf
+++ main.tf
@@ -80,50 +80,20 @@ resource "aws_ecr_repository_policy" "this" {
               : data.aws_iam_policy_document.ecs_ecr_read_perms.json}"
 }
 
-# aws_ecr_lifecycle_policy_rule is used as template for a rule of the lifecycle policy
-locals {
-  aws_ecr_lifecycle_policy_rule = {
-    rulePriority = "$${priority}"
-    description  = "Rotate images after amount of: $${max_image_count} is reached for prefix $${prefix}"
+# Interpolate the rule_priority with the count.index + 1
+# The idea was to use template_vars but they do not render, hence the replace
+data "template_file" "lifecycle_policy_rules" {
+  count = "${(var.create ? 1 : 0 ) * var.lifecycle_policy_rules_count}"
 
-    selection = {
-      tagStatus     = "tagged"
-      tagPrefixList = ["$${prefix}"]
-      countType     = "imageCountMoreThan"
-      countNumber   = "$${max_image_count}"
-    }
-
-    action = {
-      type = "expire"
-    }
-  }
-}
-
-# Create a policy rule per stage
-# The jsonencoded aws_ecr_lifecycle_policy_rule will be used as template, the variables will be replaced
-# A policy will be made per available prefix
-data "template_file" "lifecycle_policy" {
-  count = "${(var.create ? 1 : 0 ) * length(var.prefixes)}"
-
-  template = "${jsonencode(local.aws_ecr_lifecycle_policy_rule)}"
-
-  vars {
-    priority = "${count.index + 1}"
-    prefix   = "${var.prefixes[count.index]}"
-
-    # If there is no count defined in the map var.prefixes_pecific_max_count, we take the var.default_max_image_count
-    max_image_count = "${lookup(var.prefixes_specific_max_count,var.prefixes[count.index], var.default_max_image_count)}"
-  }
+  template = "${replace( var.lifecycle_policy_rules[count.index], "priority:replace:this",( count.index + 1) )}"
 }
 
 # The final rendered lifecycle_policy will be regexed to remove the double quotes surrounding strings
 resource "aws_ecr_lifecycle_policy" "this" {
-  count      = "${var.create ? 1 : 0 }"
+  count      = "${var.create && var.lifecycle_policy_rules_count > 0 ? 1 : 0 }"
   repository = "${aws_ecr_repository.this.name}"
 
-  policy = "${replace(
-               replace("{\"rules\": [${join(",",data.template_file.lifecycle_policy.*.rendered)}]}",
+  policy = "${replace("{\"rules\": [${join(",",data.template_file.lifecycle_policy_rules.*.rendered)}]}",
 		 "/\"(true|false|[[:digit:]]+)\"/", "$1"
-	), "string:", ""
-      )}"
+	)}"
 }
