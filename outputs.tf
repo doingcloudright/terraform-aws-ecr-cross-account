06b84fd6957cd1a09d5f9fa0f062867317289700
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2018-08-22 12:52:15 -0400

    add an output for the ECR repo's ARN

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "arn" {
+  value       = "${element(concat(aws_ecr_repository.this.*.arn, list("")), 0)}"
+  description = "Registry ARN"
+}
+
 output "registry_id" {
   value       = "${element(concat(aws_ecr_repository.this.*.registry_id, list("")), 0)}"
   description = "Registry id"
