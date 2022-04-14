626aec16c5b4ff614f9bb90a25d3776540c664f1
Diego OJ <diego.ojeda@binbash.com.ar>
2022-04-14 15:29:32 -0300

    Make sure there is always an output even when the templates render nothing

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "repository_arn" {
-  value       = element(concat(aws_ecr_repository.this.*.arn, []), 0)
+  value       = element(concat(aws_ecr_repository.this.*.arn, [""]), 0)
   description = "Repository ARN"
 }
 
 output "repository_name" {
-  value       = element(concat(aws_ecr_repository.this.*.name, []), 0)
+  value       = element(concat(aws_ecr_repository.this.*.name, [""]), 0)
   description = "Repository name"
 }
 
 output "registry_id" {
-  value       = element(concat(aws_ecr_repository.this.*.registry_id, []), 0)
+  value       = element(concat(aws_ecr_repository.this.*.registry_id, [""]), 0)
   description = "Registry ID"
 }
 
 output "registry_url" {
-  value       = element(concat(aws_ecr_repository.this.*.repository_url, []), 0)
+  value       = element(concat(aws_ecr_repository.this.*.repository_url, [""]), 0)
   description = "Registry URL"
 }
